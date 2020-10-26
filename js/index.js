d04c2ae3ebc057a7acec92b10d6bbf1eb5f99d28
RaveElevator <51494031+RaveElevator@users.noreply.github.com>
2020-10-26 18:54:48 -0400

    Fear and Greed Section

---
+++ js/index.js
@@ -0,0 +1,30 @@
+{
+    "data": {
+       "1": {
+          "id": 1,
+          "name": "Bitcoin",
+          "symbol": "BTC",
+          "website_slug": "bitcoin",
+          "rank": 1,
+          "circulating_supply": 17277650,
+          "total_supply": 17277650,
+          "max_supply": 21000000,
+          "quotes": {
+             "USD": {
+                "price": 6420.75294203,
+                "volume_24h": 4234633625.35,
+                "market_cap": 110935522069,
+                "percentage_change_1h": 0.08,
+                "percentage_change_24h": 0.89,
+                "percentage_change_7d": -0.26
+             }
+          },
+          "last_updated": 1537430662
+       }
+    },
+    "metadata": {
+       "timestamp": 1537430662,
+       "num_cryptocurrencies": 935,
+       "error": null
+    }
+ }
